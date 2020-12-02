d297d2938603848f9563a8b165e93635b0946bdb
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-12-01 22:19:28 -0500

    Exercise-4.2 completed

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,22 @@
 const request = require('request-promise');
 
+const apiObj = {
+  uri: 'https://api.tronalddump.io/random/quote',
+  headers: {
+      'Accept': 'application/json'
+  }
+}
+
 const getTronaldDumpQuote = async () => {
-  // write write write
-};
+  try {
+    const response = await request(apiObj);
+    const parsedResponse = JSON.parse(response);
+    const joke = parsedResponse.value;
+    return joke;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//Testing
+getTronaldDumpQuote().then((data) => console.log(data));
