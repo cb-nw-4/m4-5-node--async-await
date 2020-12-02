5e2d0ed33a0d1cc7a1d01e7a78352571617cd21a
Doug Wright <dw@nof49.ca>
2020-12-02 15:52:07 -0500

    Completed exercise 4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,14 @@
 const request = require('request-promise');
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  try {
+    const response = await request('https://geek-jokes.sameerkumar.website/api?format=json');
+    const jokeObj = JSON.parse(response);
+
+    return jokeObj.joke;
+  } catch (err) {
+    console.log('Error: ', err);
+  }
 };
+
+getGeekJoke().then(data => console.log(data));
