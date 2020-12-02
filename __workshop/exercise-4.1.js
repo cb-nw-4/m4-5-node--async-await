5432379b1698ff38ec69416d6b0187ca624674f0
Sandra <sandrabess@hotmail.com>
2020-12-02 14:49:39 -0500

    exercise 4 completed

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,23 @@
 const request = require("request-promise");
 
-// getDadJoke
 
-// 4.1
-// getDadJoke().then((data) => console.log(data));
+const getDadJoke = async () => {
+
+    const options = {
+        uri: 'https://icanhazdadjoke.com/',        
+        headers: {
+            'User-Agent': 'Request-Promise',
+            "Accept": "application/json"
+        }
+    };
+
+    try {
+        const joke =  await request(options) // 1
+        const parsedJoke = JSON.parse(joke);
+        return parsedJoke.joke;
+    } catch (err) {
+        return err.error ? JSON.parse(err.error) : err;
+    }
+  };
+
+getDadJoke().then((data) => console.log(data));
--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,14 @@
 const request = require('request-promise');
 
-const getGeekJoke = async () => {
-  // ha hahahh
+const getGeekJoke = async () => {  
+  try {
+      const joke =  await request('https://geek-jokes.sameerkumar.website/api?format=json') 
+      const parsedJoke = JSON.parse(joke);
+      return parsedJoke.joke;
+  } catch (err) {
+      return err.error;
+  }
 };
+
+getGeekJoke().then(data=>(console.log(data)));
+
