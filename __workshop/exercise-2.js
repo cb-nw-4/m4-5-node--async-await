8ac90b0d08aa6a1a9ff1b7c5fa48456d0e9056cb
chris shino <chris.shino@hotmail.com>
2020-12-01 19:53:36 -0500

    done 2

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -5,11 +5,12 @@ const request = require('request-promise');
 
 const getIssPosition = async () => {
   try {
-    const response = await request(/* fill this in */);
-    const issLocation = JSON.parse(/* fill this in */);
+    const response = await request('http://api.open-notify.org/iss-now.json');
+    const issLocation = JSON.parse(response);
+    // console.log(issLocation)
     return {
-      lat: /* fill this in */,
-      lng: /* fill this in */,
+      lat: issLocation.latitude,
+      lng: issLocation.longitude,
     };
   } catch (err) {
     console.log('Error: ', err);
