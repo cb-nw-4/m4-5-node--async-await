85a53c598fc0857507204255b1515dc822dba675
chris shino <chris.shino@hotmail.com>
2020-12-01 20:45:48 -0500

    done 4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -2,5 +2,14 @@ const request = require("request-promise");
 
 // getDadJoke
 
+const getDadJoke = async () => {
+  let response = await request({
+    uri: "https://icanhazdadjoke.com/",
+    headers: { Accept: "application/json" },
+  });
+  const parsed = JSON.parse(response)
+  return parsed.joke
+};
+
 // 4.1
-// getDadJoke().then((data) => console.log(data));
+getDadJoke().then((data) => console.log(data));
