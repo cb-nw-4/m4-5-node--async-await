2f939af7baa719cfd93cf59410aac858c838ccdd
chris shino <chris.shino@hotmail.com>
2020-12-01 21:02:21 -0500

    done 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -2,4 +2,9 @@ const request = require('request-promise');
 
 const getTronaldDumpQuote = async () => {
   // write write write
+  let response = await request({uri: 'https://api.tronalddump.io/random/quote', headers:{accept:'application/json'}})
+  let parsed = JSON.parse(response)
+  console.log(parsed.value)
 };
+
+getTronaldDumpQuote()
