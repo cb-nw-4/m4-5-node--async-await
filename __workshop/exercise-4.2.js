96b7e55cc7ee6df7e708e26b1127005c78718b1e
Doug Wright <dw@nof49.ca>
2020-12-02 15:44:41 -0500

    Completed exercise 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,18 @@
 const request = require('request-promise');
+const requestObj = {
+  uri: 'https://api.tronalddump.io/random/quote',
+  headers: { 'accept': 'application/json' }
+};
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  try {
+    const response = await request(requestObj);
+    const quoteObj = JSON.parse(response);
+    
+    return quoteObj.value;
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+getTronaldDumpQuote().then((data) => console.log(data));
