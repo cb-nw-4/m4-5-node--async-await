c28ea174a087ba65c63d1cb66788877c7a159083
Elodie L <67484805+EL-Bloom@users.noreply.github.com>
2020-12-03 14:50:59 -0500

    1- 3

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -5,15 +5,20 @@ const request = require('request-promise');
 
 const getIssPosition = async () => {
   try {
-    const response = await request(/* fill this in */);
-    const issLocation = JSON.parse(/* fill this in */);
+    const response = await request("http://api.open-notify.org/iss-now.json");
+    const issLocation = JSON.parse(response);
+    const parsedResponse = issLocation.iss_position;
     return {
-      lat: /* fill this in */,
-      lng: /* fill this in */,
+      lat: parsedResponse.latitude,
+      lng: parsedResponse.longitude,
     };
+  
   } catch (err) {
     console.log('Error: ', err);
   }
 };
 
-console.log(getIssPosition());
+const handleGetIssPosition = async () => {
+console.log( await getIssPosition());
+} 
+handleGetIssPosition();
--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -2,11 +2,30 @@
 // ----------
 
 const doublesLater = (num) => {
+  return new Promise(function(resolve) {
+    const sum = num * 2;
+    setTimeout(() => resolve(sum), 2000)
+  })
+
+};
+const handleSum = async (num) => {
+try {
+  const ans = await doublesLater(num);
+  console.log(ans);
+}
+catch (err) {
+console.log("error")
+}
+}
+
+
+
+handleSum(20);
+
+
   // 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
   // 2. returns the double of the num
-};
 
 // 3. handleSum function (async/await)
 
 // 4. verification
-handleSum(10).then((ans) => console.log(ans));
