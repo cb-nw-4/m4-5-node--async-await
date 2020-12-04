ea271030964cdc09c026acc50ef03b235e8da36b
Joni Sadler <joni.anne.sadler@gmail.com>
2020-12-03 20:31:08 -0500

    Exercise 4.2 done

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,18 @@
 const request = require('request-promise');
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  try {
+    let result = await request({
+      uri: "https://api.tronalddump.io/random/quote",
+      headers: {
+        "Accept": "application/json"
+      }
+    })
+    let quote = JSON.parse(result);
+    return quote.value;
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+getTronaldDumpQuote().then((response) => console.log(response));
