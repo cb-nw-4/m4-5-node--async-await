f5bab09769a8c0832982ac6215cfdd6e7d4df545
Joni Sadler <joni.anne.sadler@gmail.com>
2020-12-03 20:01:43 -0500

    Exercise 4.1 done

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,34 @@
+const { response } = require("express");
 const request = require("request-promise");
 
 // getDadJoke
 
+// const getDadJoke = () => {
+//   return request("https://icanhazdadjoke.com/")
+//   .then((response) => {
+//     let parsedJoke = JSON.parse(response);
+//     console.log(parsedJoke);
+//     return parsedJoke.joke;
+//   })
+//   .catch((err) => console.log('Error', err));
+// };
+
+const getDadJoke = async () => {
+  try {
+    let options = await request({
+      uri: 'https://icanhazdadjoke.com/',
+      headers: {
+        "Accept": "application/json"
+      }
+    })
+    let parsedJoke = JSON.parse(options);
+    console.log(parsedJoke.joke);
+    return parsedJoke.joke;
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
 // 4.1
-// getDadJoke().then((data) => console.log(data));
+getDadJoke().then((data) => console.log(data));
