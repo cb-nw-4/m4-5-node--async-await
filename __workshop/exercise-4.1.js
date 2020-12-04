1e45193bfb2b23f96d9cc2baa9b44142c7a8c89e
Vanessa Chan <chw.vanessa@gmail.com>
2020-12-04 17:29:38 -0500

    Ex 4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,18 @@
 const request = require("request-promise");
 
 // getDadJoke
+const getDadJoke = () =>{
+    var options = {
+        uri: 'https://icanhazdadjoke.com/',
+        headers: {
+            'Accept': 'application/json'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+
+    return request(options).then((res)=>res.joke);
+
+};
 
 // 4.1
-// getDadJoke().then((data) => console.log(data));
+getDadJoke().then((data) => console.log(data));
