c44772f40e6c5a4ebc6373311725c6a6384ca6ea
“Winnie <chanwinnie.wc@gmail.com>
2020-12-07 02:48:36 -0500

    Exercise 4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -2,4 +2,15 @@ const request = require('request-promise');
 
 const getGeekJoke = async () => {
   // ha hahahh
+  try {
+    const result = await request('https://geek-jokes.sameerkumar.website/api?format=json');
+    const resultObj = JSON.parse(result);
+    const geekJoke = resultObj.joke;
+    console.log(geekJoke);
+    return geekJoke;
+  } catch(err) {
+    console.log(err)
+  }
 };
+
+getGeekJoke();
