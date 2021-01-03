0adca9dd970277ec2340aca1dc3c99e0d6e3d9e4
Alexandre <alex_18h@hotmail.com>
2021-01-03 18:25:05 -0500

    exercise 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -2,4 +2,8 @@ const request = require('request-promise');
 
 const getTronaldDumpQuote = async () => {
   // write write write
+  let quote = await request({uri: 'https://api.tronalddump.io/random/quote', headers:{accept:'application/json'}})
+  let parsed = JSON.parse(quote)
+  console.log(parsed.value)
 };
+getTronaldDumpQuote()
